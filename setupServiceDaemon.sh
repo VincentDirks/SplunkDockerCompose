07dfdd0a81b5d7602576441879fb85e35938c832
VincentDirks <vincent@dirksonline.net>
2024-04-30 11:50:35 +1200

    Try chmod +x setupServiceDaemon.sh

--- setupServiceDaemon.sh
+++ setupServiceDaemon.sh
@@ -1,4 +1,4 @@
-# This copies the 
+# This copies the service file to the correct location, sets the splunk password, and reloads the services. 
 cp /root/application/docker-compose.service /etc/systemd/system/docker-compose.service
 sed -i "s/\${SPLUNK_PASSWORD}/${SPLUNK_PASSWORD}/g" /etc/systemd/system/docker-compose.service
 cat /etc/systemd/system/docker-compose.service
