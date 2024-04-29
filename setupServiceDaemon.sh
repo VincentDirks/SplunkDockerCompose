179fbbcb42c077208aaf51d34d633ec90586bd01
VincentDirks <vincent@dirksonline.net>
2024-04-30 11:59:22 +1200

    remove cat of service file

--- setupServiceDaemon.sh
+++ setupServiceDaemon.sh
@@ -1,5 +1,4 @@
 # This copies the service file to the correct location, sets the splunk password, and reloads the services. 
 cp /root/application/docker-compose.service /etc/systemd/system/docker-compose.service
 sed -i "s/\${SPLUNK_PASSWORD}/${SPLUNK_PASSWORD}/g" /etc/systemd/system/docker-compose.service
-cat /etc/systemd/system/docker-compose.service
 systemctl daemon-reload
